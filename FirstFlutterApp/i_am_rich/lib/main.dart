2a7878047a8e9a73e77376dd73093192896feb7f
Juan Chalita <chalajr2@gmail.com>
2021-10-16 14:59:47 -0500

    Completed Section 3 chapter 27

---
+++ FirstFlutterApp/i_am_rich/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+//The main function is starting pont for all our Flutter apps.
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text(
+            'I Am Rich',
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
